2f65e0193b3e74dc29dfadf9245253e9c27391e9
MEHRSHAD-OSG <95499396+MEHRSHAD-OSG@users.noreply.github.com>
2024-07-27 00:46:40 -0700

    models.py

--- accounts/models.py
+++ accounts/models.py
@@ -14,14 +14,12 @@ class UserManager(BaseUserManager):
             raise ValueError("full name can't be empty")
         user = self.model(email=self.normalize_email(email),phone_number=phone_number,full_name=full_name)
         user.set_password(password)
-        # using=self.db => for use active db
         user.save(using=self._db)
         return user
 
     def create_superuser(self,phone_number,full_name,email,password):
         user = self.create_user(phone_number=phone_number,full_name=full_name,email=email,password=password)
         user.is_admin = True
-        # user.is_superuser = True
         user.save(using=self._db)
         return user
 
@@ -33,22 +31,19 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     objects = UserManager()
-    # anything in username field must be unique was true
     USERNAME_FIELD = 'phone_number'
-    # just for createsuperuser
     REQUIRED_FIELDS = ['email', 'full_name']
 
     def __str__(self):
         return self.email
-    # for user permissions
     def has_perm(self,perm,obj=None):
         return True
 
-    # user can use models or not
+
     def has_module_perms(self,app_label):
         return True
 
-    # for convert to boolean field
+
     @property
     def is_staff(self):
         return self.is_admin
