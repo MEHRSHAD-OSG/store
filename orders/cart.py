3882edca97826bc69314876f89ec8bd4c047cfca
MEHRSHAD-OSG <95499396+MEHRSHAD-OSG@users.noreply.github.com>
2024-07-27 00:43:45 -0700

    cart.py

--- orders/cart.py
+++ orders/cart.py
@@ -5,9 +5,7 @@ CART_SESSION_ID = 'cart'
 
 class Cart:
     def __init__(self,request):
-        # get all session
         self.session = request.session
-        # separate cart session
         cart = self.session.get(CART_SESSION_ID)
         if not cart:
             cart = self.session[CART_SESSION_ID] = {}
@@ -15,33 +13,24 @@ class Cart:
 
     def __iter__(self):
         product_ids = self.cart.keys()
-        # search in ids and check exists or not
         products = Product.objects.filter(id__in= product_ids)
         cart = self.cart.copy()
         for product in products:
-            # create product key in session
             cart[str(product.id)]['product'] = product
 
         for item in cart.values():
-            # create total_price in item
             item['total_price'] = int(item['price']) * item['quantity']
             yield item
 
     def __len__(self):
-        # this func for countable this class
         return sum(item['quantity'] for item in self.cart.values())
 
-    """
-    this func args send from client
-    """
+
     def add(self,product,quantity):
-        # in db
         product_id = str(product.id)
         if product_id not in self.cart:
-            # product_id is a name
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
         self.cart[product_id]['quantity'] += quantity
-        # because change quantity must save
         self.save()
 
     def remove(self, product):
