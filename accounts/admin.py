f863f365b475fc8e5795302d795453c023b72026
MEHRSHAD-OSG <95499396+MEHRSHAD-OSG@users.noreply.github.com>
2024-07-27 00:47:04 -0700

    admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -31,12 +31,7 @@ class UserAdmin(BaseUserAdmin):
 	ordering = ('full_name',)
 	filter_horizontal = ()
 
-	# def get_form(self, request, obj=None, **kwargs):
-	# 	form = super().get_form(request, obj, **kwargs)
-	# 	is_superuser = request.user.is_superuser
-	# 	if not is_superuser:
-	# 		form.base_fields['is_superuser'].disabled = True
-	# 	return form
+
 
 admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
