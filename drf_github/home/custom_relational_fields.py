0bae502b1d01e4f7467224958380b5fae2756029
Mehrshad <mehrshadoshaghi92@gmail.com>
2025-04-13 09:03:29 +0330

    Done

--- drf_github/home/custom_relational_fields.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class EmailAndUsernameFields(serializers.RelatedField):
-    def to_representation(self, value):
-        # value is a object
-        return f"{value.email} - {value.username}"
